4d59bfe7bdf7f4430f16ef092698a6424347d842
elnorg <elnorg@hotmail.it>
2015-11-18 23:52:08 +0000

    First commit of a sample project

---
+++ CheckRepeatedWords/CheckRepeatedWords.cpp
@@ -0,0 +1,19 @@
+// CheckRepeatedWords.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+	string previous = " ";       // previous word;initialized to “not a word”
+	string current;              // current word
+	while (cin >> current)       // read a stream of words
+	{
+		if (previous == current) // check if the word is the same as last
+		{
+			cout << "repeated word: " << current << '\n';
+		}
+		previous = current;
+	}
+}
