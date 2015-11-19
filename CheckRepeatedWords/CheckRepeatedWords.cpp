bcb976a998ca599d7e944a7c48c4662fb3548638
elnorg <elnorg@hotmail.it>
2015-11-19 00:00:13 +0000

    Counting repeated words

--- CheckRepeatedWords/CheckRepeatedWords.cpp
+++ CheckRepeatedWords/CheckRepeatedWords.cpp
@@ -8,11 +8,16 @@ int main()
 {
 	string previous = " ";       // previous word;initialized to “not a word”
 	string current;              // current word
+	int count = 0;               // count how many times the word got repeated
 	while (cin >> current)       // read a stream of words
 	{
 		if (previous == current) // check if the word is the same as last
 		{
-			cout << "repeated word: " << current << '\n';
+			cout << "repeated word: " << current << " " << ++count << " time(s)" << '\n';
+		}
+		else
+		{
+			count = 0;   // reset the counter
 		}
 		previous = current;
 	}
